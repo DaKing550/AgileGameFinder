e465ec46e946d751f0a2d44a6bcf02bb0663a29a
alisaidmoha1 <alisaidmoha1@gmail.com>
2021-09-09 12:31:46 -0400

    Added connect

---
+++ GameFinder.Data/Game.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameFinder.Data
+{
+    public class Game
+    {
+        public int GameId { get; set; }
+    }
+}
--- GameFinder.Data/IdentityModels.cs
+++ GameFinder.Data/IdentityModels.cs
@@ -1,4 +1,7 @@
-﻿using System.Security.Claims;
+﻿using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Security.Claims;
 using System.Threading.Tasks;
 using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.EntityFramework;
@@ -28,6 +31,35 @@ namespace GameFinder.Data
         public static ApplicationDbContext Create()
         {
             return new ApplicationDbContext();
+        }
+
+        public DbSet <Game> Games { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder
+                .Conventions
+                .Remove<PluralizingTableNameConvention>();
+            modelBuilder
+                .Configurations
+                .Add(new IdentityUserLoginConfiguration())
+                .Add(new IndentityRoleConfiguration());
+        }
+    }
+
+    public class IdentityUserLoginConfiguration : EntityTypeConfiguration<IdentityUserLogin>
+    {
+        public IdentityUserLoginConfiguration()
+        {
+            HasKey(iul => iul.UserId);
+        }
+    }
+
+    public class IndentityRoleConfiguration : EntityTypeConfiguration<IdentityUserRole>
+    {
+        public IndentityRoleConfiguration()
+        {
+            HasKey(iur => iur.UserId);
         }
     }
 }
