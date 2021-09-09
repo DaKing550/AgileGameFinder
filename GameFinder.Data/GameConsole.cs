e369dc310fbab1c92b2e4f263c118b141fe44e54
David King <david.king550wil@gmail.com>
2021-09-09 16:04:23 -0400

    game console

--- GameFinder.Data/GameConsole.cs
+++ GameFinder.Data/GameConsole.cs
@@ -1,12 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace GameFinder.Data
 {
-    class GameConsole
+    public class GameConsole
     {
+        [Key]
+        public int ConsoleID { get; set; }
+        [Required]
+        public string ConsoleName { get; set; }
+
     }
 }
